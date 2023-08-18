518b52f20cdc7f5b24d2ed7e3d401acee03328f8
Hodon534 <hodon534@gmail.com>
2023-08-18 18:23:02 +0200

    initial commit

--- src/test/java/com/webshop/repository/ProductInventoryRepositoryTest.java
+++ src/test/java/com/webshop/repository/ProductInventoryRepositoryTest.java
@@ -2,7 +2,7 @@ package com.webshop.repository;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
+@DataJpaTest // Test Repository
 class ProductInventoryRepositoryTest {
 
 }
