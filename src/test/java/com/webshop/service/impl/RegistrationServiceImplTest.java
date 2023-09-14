c1a3c33bb8792b6c6a8029e53dd3ca749b221cf3
Hodon534 <hodon534@gmail.com>
2023-09-14 13:03:12 +0200

    tests added

--- src/test/java/com/webshop/service/impl/RegistrationServiceImplTest.java
+++ src/test/java/com/webshop/service/impl/RegistrationServiceImplTest.java
@@ -1,12 +1,33 @@
 package com.webshop.service.impl;
 
+import com.webshop.model.entity.UserEntity;
+import com.webshop.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class RegistrationServiceImplTest {
 
+    @Mock
+    private UserService userService;
+
+    private RegistrationServiceImpl underTest;
+
+    @BeforeEach
+    void setUp() {
+        userService = mock(UserService.class);
+        underTest = new RegistrationServiceImpl(userService);
+    }
+
     @Test
-    void register() {
+    void shouldRegisterUser() {
+        // given
+
+        // when
+
+        // then
     }
 }
